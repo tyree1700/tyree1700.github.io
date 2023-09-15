ad6a6a4f59ebea062c1df66a1c0e932768d0fdce
tyree1700 _ <tyree.smith@sciacademy.org>
2023-09-15 14:43:18 +0000

    <message about commit>

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -18,8 +18,8 @@ $(function () {
     //create walls
     createPlatform(-50, -50, canvas.width + 100, 50); //top
     createPlatform(-50, canvas.height - 10, canvas.width + 100, 200); //right
-    createPlatform(-50, -50, 50, canvas.height + 500); //bottom
-    createPlatform(canvas.width, -50, 50, canvas.height + 100);
+    createPlatfrm(-50, -50, 50, canvas.height + 500); //bottom
+    createPlatformo(canvas.width, -50, 50, canvas.height + 100);
 
     /**
      * Uncomment the loops below to add a "grid" to your platformer game's screen
